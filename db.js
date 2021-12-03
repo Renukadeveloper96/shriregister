54806d6db1a163e6f8b245fb39f8319f099be739
Renukadeveloper96 <renukamaradagi@gmail.com>
2021-12-03 22:59:54 +0530

    user

--- db.js
+++ db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/augintern')
+mongoose.connect('mongodb+srv://zomato:zomato77@cluster0.nbutl.mongodb.net/zomato?retryWrites=true&w=majority')
